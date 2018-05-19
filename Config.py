001bfec78e303a7fb00f48813e5e16a83907422e
qian-bi <2295823382@qq.com>
2018-05-19 19:22:00 +0800

    Init

---
+++ Config.py
@@ -0,0 +1,10 @@
+location_on_pc = {
+    'left_top_x': 71,
+    'left_top_y': 437,
+    'right_buttom_x': 560,
+    'right_buttom_y': 650,
+    'click_true_x': 152,
+    'click_true_y': 706,
+    'click_false_x': 350,
+    'click_false_y': 706
+}
---
+++ Util.py
@@ -0,0 +1,51 @@
+from ImgTools import *
+from PIL import Image
+import os
+import time
+import json
+
+
+def get_screenshots():
+    for i in range(50):
+        os.system('adb exec-out screencap -p > {0}th.png'.format(i))
+        time.sleep(1)
+
+
+def get_chars():
+    for img_name in os.listdir('Screenshots'):
+        img = Image.open(os.path.join('Screenshots', img_name))
+        img = binarize(img.crop([50, 280, 400, 480]))
+        h_cut_imgs = horizontal_cut(img)
+        char_list1 = vertical_cut(h_cut_imgs[0])
+        print(char_list1)
+        char_list2 = vertical_cut(h_cut_imgs[1])
+        print(char_list2)
+        for char in char_list1:
+            char.show()
+            pic_name = input('name:')
+            if pic_name != '':
+                char.save('Characters/{0}.png'.format(pic_name))
+        for char in char_list2:
+            char.show()
+            pic_name = input('name:')
+            if pic_name != '':
+                char.save('Characters/{0}.png'.format(pic_name))
+
+
+def get_hashfile():
+    hash_vals = {}
+    for char in os.listdir('Characters'):
+        img = Image.open(os.path.join('Characters', char))
+        hash_val = hashing(binarize(img))
+        char_name = char[:-4]
+        hash_vals[char_name] = hash_val
+
+    with open('HashFiles/hash.json', 'w') as fp:
+        json.dump(hash_vals, fp)
+
+    return True
+
+
+if __name__ == '__main__':
+    # get_chars()
+    get_hashfile()
